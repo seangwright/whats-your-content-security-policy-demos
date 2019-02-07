233332d5708748a2db42c45ea3ad8a72e731b54c
Sean Wright <sean@seangwright.me>
2019-02-06 23:23:38 -0500

    fix(site): prod webpack build, remove react dist from git

--- src/vulnerable-site/webpack.config.js
+++ src/vulnerable-site/webpack.config.js
@@ -5,7 +5,6 @@ console.log(__dirname);
 const config = {
   context: __dirname,
   entry: './react-app/index.jsx',
-  devtool: 'cheap-eval-source-map',
   output: {
     path: path.join(__dirname, 'public/javascripts'),
     filename: 'password-app.js'
@@ -14,9 +13,15 @@ const config = {
     extensions: ['.jsx', '.js', '.json']
   },
   module: {
-    rules: [{ test: /\.jsx?$/, loader: 'babel-loader' }]
+    rules: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader'
+    }]
   },
-  mode: 'development'
+  mode: 'development',
+  node: {
+    global: false
+  }
 };
 
 module.exports = config;
